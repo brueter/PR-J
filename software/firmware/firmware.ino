ab9c0039664b1162a00db2908ab1fdf15b7ec0b7
Juniper Rueter <brueter918@gmail.com>
2023-10-07 21:41:57 -0400

    updated 202 firmware to add software low pass filtering and median filtering

--- software/firmware/firmware.ino
+++ software/firmware/firmware.ino
@@ -3,113 +3,82 @@
 const byte DOUT = 0;
 const byte CLK = 4;
 const byte LED = 1;
+const int MEDIAN_FILTER_SIZE = 3; // Adjust this value based on your requirements
+const int LOW_PASS_FILTER_SIZE = 10;
 
 void setup() {
-  digitalWrite(LED, LOW);
-  Wire.begin(0x69);                 // I2C address
-  Wire.onRequest(tx);
-  Wire.onReceive(rx);
+  Wire.begin(8);
+  Wire.onRequest(rx);
+  pinMode(DOUT, INPUT_PULLUP);
+  pinMode(CLK, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-
+  
 }
 
-inline void tx(void) {
-  Wire.write(read());
+inline void rx(void) {
+  unsigned long smoothedValue = readAndFilter();
+  byte longBytes[4];
+  memcpy(longBytes, &smoothedValue, 4);
+  Wire.write(longBytes, 4);
 }
 
-unsigned long lowThreshold = 0;
-unsigned long highThreshold = 0;
-
-#define OP_LED_OFF      0
-#define OP_LED_LMH      1
-#define OP_LED_HML      2
-#define OP_LED_HLH      3
-
-#define OP_STRAIN_LOW   4
-#define OP_STRAIN_HIGH  5
-
-int ledEncoding = 0;
-
-#define IS_OP_LED(opCode) (opCode < OP_STRAIN_LOW)
-
-void rx(int bytes) {
-  while(Wire.available()) {
-    byte opCode = Wire.read();
-
-    if (IS_OP_LED(opCode)) {
-    ledEncoding = opCode;
-    } else {
-      unsigned long value = 0;
-      
-      for (byte i = 0 ; i < 2 ; i++) {
-        value += Wire.read();
-        
-        if(i == 1) break;
-
-        value = value << 8;
-      }
-      
-      if (opCode == OP_STRAIN_LOW) {
-          lowThreshold = value;
-      } else {
-          highThreshold = value;
-      }
-    }
+unsigned long readAndFilter() {
+  unsigned long buffer[MEDIAN_FILTER_SIZE];
+  
+  for (int i = 0; i < MEDIAN_FILTER_SIZE; i++) {
+    buffer[i] = read();
   }
-}
 
-int ledBrightness = 0;
+  unsigned long median = calculateMedian(buffer, MEDIAN_FILTER_SIZE);
+
+  static unsigned long filterBuffer[LOW_PASS_FILTER_SIZE];
+  static int index = 0;
+  static unsigned long sum = 0;
+  
+  filterBuffer[index] = median;
+  sum += filterBuffer[index] - filterBuffer[(index + 1) % LOW_PASS_FILTER_SIZE];
+  index = (index + 1) % LOW_PASS_FILTER_SIZE;
+  
+  return sum / LOW_PASS_FILTER_SIZE;
+}
 
 unsigned long read() {
-  int ledWrite = 0;
   unsigned long output = 0;
   byte i;
 
   digitalWrite(CLK, LOW);
+  
+  while (digitalRead(DOUT) == HIGH);
 
-  while (digitalRead(DOUT));         // wait for DOUT to go low, indicating ready to output
-
-  for (i = 0 ; i < 24 ; i++) {       // send 25 pulses, indicating to read from channel A with a gain of 128
-    digitalWrite(CLK, HIGH);         // clock high
-    output = output << 1;            // bit shift left
-    digitalWrite(CLK, LOW);          // clock low
-    if (digitalRead(DOUT)) output++; // set last bit of output based on DOUT
+  for (i = 0; i < 24; i++) {
+    digitalWrite(CLK, HIGH);
+    output = output << 1;
+    digitalWrite(CLK, LOW);
+    if (digitalRead(DOUT)) output++;
   }
 
-  digitalWrite(CLK, HIGH);           // finish signal
-  output = output ^ 0x800000;        // clipping
-  digitalWrite(CLK, LOW);            // keep CLK low to prevent sleep
-
-  switch(ledEncoding) {
-    case OP_LED_LMH:
-      ledWrite = ((output - lowThreshold) << 8) / (highThreshold - lowThreshold);
-      break;
-    case OP_LED_HML:
-      ledWrite = 255 - (((output - lowThreshold) << 8) / (highThreshold - lowThreshold));
-      break;
-    case OP_LED_HLH:
-      unsigned long middle;
-      middle = lowThreshold + ((highThreshold - lowThreshold) >> 1);
-      
-      if (output <= middle) {
-        ledWrite = 255 - ((output - lowThreshold) << 8) / (middle - lowThreshold);
-      } else {
-        ledWrite = ((output - middle) << 8) / (highThreshold - middle);
-      }
-      break;
-    default:
-      ledWrite = 0;
-      break;
-  }
-
-  ledWrite = ledWrite < 0 ? 0 : (ledWrite > 255 ? 255 : ledWrite);  // Clip at 0 and 255
-
-  if(ledWrite != ledBrightness) {
-    analogWrite(LED, ledWrite);
-    ledBrightness = ledWrite;
-  }
-
+  digitalWrite(CLK, HIGH);
+  output = output ^ 0x800000;
+  digitalWrite(CLK, LOW);
+  
   return output;
+}
+
+unsigned long calculateMedian(unsigned long* values, int length) {
+  // Sort the array to find the median
+  for (int i = 0; i < length - 1; i++) {
+    for (int j = i + 1; j < length; j++) {
+      if (values[i] > values[j]) {
+        unsigned long temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+      }
+    }
+  }
+
+  // The median is at the middle of the sorted array
+  return values[length / 2];
 }
