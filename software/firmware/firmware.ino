78204a6fb8dce91a0ea59bbf3aafc8f295e2a404
Juniper Rueter <brueter918@gmail.com>
2023-10-11 19:57:15 -0400

    added software to analyze data from an excel spreadsheet alongside various filtering methods

--- software/firmware/firmware.ino
+++ software/firmware/firmware.ino
@@ -3,6 +3,8 @@
 const byte DOUT = 0;
 const byte CLK = 4;
 const byte LED = 1;
+const int MEDIAN_FILTER_SIZE = 3; // Adjust this value based on your requirements
+const int LOW_PASS_FILTER_SIZE = 10;
 
 void setup() {
   Wire.begin(8);
@@ -12,21 +14,37 @@ void setup() {
   pinMode(LED, OUTPUT);
 }
 
-union LongBuffer {
-  unsigned long longNumber;
-  byte longBytes[3];
-};
-
-LongBuffer buffer;
-
 void loop() {
   
 }
 
 inline void rx(void) {
-  buffer.longNumber = read();
-  Wire.write(buffer.longBytes, 3);
+  unsigned long smoothedValue = readAndFilter();
+  byte longBytes[4];
+  memcpy(longBytes, &smoothedValue, 4);
+  Wire.write(longBytes, 4);
 }
+
+unsigned long readAndFilter() {
+  unsigned long buffer[MEDIAN_FILTER_SIZE];
+  
+  for (int i = 0; i < MEDIAN_FILTER_SIZE; i++) {
+    buffer[i] = read();
+  }
+
+  unsigned long median = calculateMedian(buffer, MEDIAN_FILTER_SIZE);
+
+  static unsigned long filterBuffer[LOW_PASS_FILTER_SIZE];
+  static int index = 0;
+  static unsigned long sum = 0;
+  
+  filterBuffer[index] = median;
+  sum += filterBuffer[index] - filterBuffer[(index + 1) % LOW_PASS_FILTER_SIZE];
+  index = (index + 1) % LOW_PASS_FILTER_SIZE;
+  
+  return sum / LOW_PASS_FILTER_SIZE;
+}
+
 unsigned long read() {
   unsigned long output = 0;
   byte i;
@@ -47,4 +65,20 @@ unsigned long read() {
   digitalWrite(CLK, LOW);
   
   return output;
+}
+
+unsigned long calculateMedian(unsigned long* values, int length) {
+  // Sort the array to find the median
+  for (int i = 0; i < length - 1; i++) {
+    for (int j = i + 1; j < length; j++) {
+      if (values[i] > values[j]) {
+        unsigned long temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+      }
+    }
+  }
+
+  // The median is at the middle of the sorted array
+  return values[length / 2];
 }
